96956d74894abe64c0c3519eaf80f0468f4ac2ab
farex4sure <faruqhassan176@gmail.com>
2022-10-30 14:26:58 +0100

    farex new update

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -21,57 +21,57 @@ const Hero = () => {
         <section className="promo pb-8">
             <div className="slider-wrap flex flex-col justify-center items-center gap-4">
                 <div className='w-full relative'>
-                    <div id="carouselExampleCaptions" class="carousel slide relative" data-bs-ride="carousel">
-                        <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-label="Slide 1" ></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" class="active" aria-label="Slide 2" ></button>
-                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" class="active" aria-label="Slide 3" ></button>
+                    <div id="carouselExampleCaptions" className="carousel slide relative" data-bs-ride="carousel">
+                        <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-label="Slide 1" ></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" className="active" aria-label="Slide 2" ></button>
+                            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" className="active" aria-label="Slide 3" ></button>
                         </div>
-                        <div class="carousel-inner relative w-full overflow-hidden">
-                            <div class="h-full max-h-[400px] carousel-item active relative float-left w-full">
-                                <img src={slide1} class="block w-full object-cover hidden lg:block" alt="..." />
-                                <img src={slide1} class="block w-full h-full object-cover lg:hidden" alt="..." />
-                                <div class="hidden flex flex-col lg:flex-row lg:px-8 carousel-caption absolute text-green-700 left-0 top-0 bg-gray-100 bg-opacity-25 w-full">
-                                    <div class="w-full lg:w-2/3 h-full flex flex-col lg:flex-row">
-                                        <div class="flex flex-col items-center gap-y-4 md:gap-y-6 p-2">
-                                            <h5 class="text-xl md:2xl lg:text-6xl">Register and get your PVC today</h5>
+                        <div className="carousel-inner relative w-full overflow-hidden">
+                            <div className="h-full max-h-[400px] carousel-item active relative float-left w-full">
+                                <img src={slide1} className="block w-full object-cover hidden lg:block" alt="..." />
+                                <img src={slide1} className="block w-full h-full object-cover lg:hidden" alt="..." />
+                                <div className="hidden flex flex-col lg:flex-row lg:px-8 carousel-caption absolute text-green-700 left-0 top-0 bg-gray-100 bg-opacity-25 w-full">
+                                    <div className="w-full lg:w-2/3 h-full flex flex-col lg:flex-row">
+                                        <div className="flex flex-col items-center gap-y-4 md:gap-y-6 p-2">
+                                            <h5 className="text-xl md:2xl lg:text-6xl">Register and get your PVC today</h5>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="h-full max-h-[400px] carousel-item relative float-left w-full">
-                                <img src={slide2} class="block w-full object-cover hidden lg:block" alt="..." />
-                                <img src={slide2} class="block w-full h-full object-cover lg:hidden" alt="..." />
-                                <div class="hidden flex flex-col lg:flex-row lg:px-8 carousel-caption absolute text-green-500 left-0 top-0 bg-gray-100 bg-opacity-25 w-full">
-                                    <div class="w-full lg:w-2/3 h-full flex flex-col lg:flex-row">
-                                        <div class="flex flex-col items-center gap-y-4 md:gap-y-6 p-2">
-                                            <h5 class="text-xl md:2xl lg:text-6xl">Register and get your PVC today</h5>
-                                            <p class="text-base md:text-xl lg:text-3xl text-slate-50 bg-green-500 bg-opacity-50 px-4 py-2 rounded-lg">
+                            <div className="h-full max-h-[400px] carousel-item relative float-left w-full">
+                                <img src={slide2} className="block w-full object-cover hidden lg:block" alt="..." />
+                                <img src={slide2} className="block w-full h-full object-cover lg:hidden" alt="..." />
+                                <div className="hidden flex flex-col lg:flex-row lg:px-8 carousel-caption absolute text-green-500 left-0 top-0 bg-gray-100 bg-opacity-25 w-full">
+                                    <div className="w-full lg:w-2/3 h-full flex flex-col lg:flex-row">
+                                        <div className="flex flex-col items-center gap-y-4 md:gap-y-6 p-2">
+                                            <h5 className="text-xl md:2xl lg:text-6xl">Register and get your PVC today</h5>
+                                            <p className="text-base md:text-xl lg:text-3xl text-slate-50 bg-green-500 bg-opacity-50 px-4 py-2 rounded-lg">
                                                 Join our celebrities and millions of other well-meaning Nigerians to participate in the voting process.
                                             </p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div class="h-full max-h-[400px] carousel-item relative float-left w-full">
-                                <img src={slide3} class="block w-full object-cover hidden lg:block" alt="..." />
-                                <img src={slide3} class="block w-full h-full object-cover lg:hidden" alt="..." />
-                                <div class="hidden flex flex-col lg:flex-row lg:px-8 carousel-caption absolute text-green-500 left-0 top-0 bg-gray-100 bg-opacity-25 w-full">
-                                    <div class="w-full lg:w-2/3 h-full flex flex-col lg:flex-row">
-                                        <div class="flex flex-col items-center gap-y-4 md:gap-y-6 p-2">
-                                            <h5 class="text-xl md:2xl lg:text-6xl">SabiVoters Challenge</h5>
+                            <div className="h-full max-h-[400px] carousel-item relative float-left w-full">
+                                <img src={slide3} className="block w-full object-cover hidden lg:block" alt="..." />
+                                <img src={slide3} className="block w-full h-full object-cover lg:hidden" alt="..." />
+                                <div className="hidden flex flex-col lg:flex-row lg:px-8 carousel-caption absolute text-green-500 left-0 top-0 bg-gray-100 bg-opacity-25 w-full">
+                                    <div className="w-full lg:w-2/3 h-full flex flex-col lg:flex-row">
+                                        <div className="flex flex-col items-center gap-y-4 md:gap-y-6 p-2">
+                                            <h5 className="text-xl md:2xl lg:text-6xl">SabiVoters Challenge</h5>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <button class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev" >
-                            <span class="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                            <span class="visually-hidden">Previous</span>
+                        <button className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev" >
+                            <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
                         </button>
-                        <button class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-                            <span class="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
-                            <span class="visually-hidden">Next</span>
+                        <button className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                            <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
                         </button>
                     </div>
                 </div>
