4b6e0cd81f1ed43e3a31b279db42dcf3385c5750
farex4sure <faruqhassan176@gmail.com>
2022-10-31 12:55:34 +0100

    farex new update

--- src/views/games/GamesIndex.js
+++ src/views/games/GamesIndex.js
@@ -17,7 +17,7 @@ const GamesIndex = () => {
             <div className='w-full py-5 grid md:grid-cols-2 lg:grid-cols-3 gap-4 md:gap-8 p-4 sm:p-6 md:p-8'>
                 <div className='flex flex-col border-2 border-dashed border-green-500 rounded-xl shadow-md p-2 overflow-hidden cursor-pointer'>
                     <div className='text-center text-3xl text-green-500 py-2' style={{fontFamily: 'Gochi Hand'}}>
-                        <h3>Imago Quad</h3>
+                        <h3>4 Pictures One Word</h3>
                     </div>
                     <Link to='/games/imago_quad'>
                         <img className='w-full h-72 md:h-64 rounded-b-xl' alt='' src={imago} />
@@ -25,7 +25,7 @@ const GamesIndex = () => {
                 </div>
                 <div className='flex flex-col border-2 border-dashed border-red-400 rounded-xl shadow-md p-2 overflow-hidden cursor-pointer'>
                     <div className='text-center text-3xl text-red-400 py-2' style={{fontFamily: 'Gochi Hand'}}>
-                        <h3>Forest Game</h3>
+                        <h3>Memory Boxes</h3>
                     </div>
                     <a target='_blank' rel='noreferrer' href='https://learncha.mybluemix.net/games/forest_game'>
                         <img className='w-full h-72 md:h-64 rounded-b-xl'  alt='' src='https://images.pexels.com/photos/1337382/pexels-photo-1337382.jpeg?auto=compress&cs=tinysrgb&w=600' />
@@ -33,7 +33,7 @@ const GamesIndex = () => {
                 </div>
                 <div className='flex flex-col border-2 border-dashed border-yellow-500 rounded-xl shadow-md p-2 overflow-hidden cursor-pointer'>
                     <div className='text-center text-3xl text-yellow-500 py-2' style={{fontFamily: 'Gochi Hand'}}>
-                        <h3>LD Learn</h3>
+                        <h3>Math and Climate</h3>
                     </div>
                     <a target='_blank' rel='noreferrer' href='https://goeroeg.github.io/ld-learn/apps/learn/ld-learn.html'>
                         <img className='w-full h-72 md:h-64 rounded-b-xl'  alt='' src='https://images.pexels.com/photos/9085647/pexels-photo-9085647.jpeg?auto=compress&cs=tinysrgb&w=600' />
